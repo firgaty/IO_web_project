d1beccbec9e083e2c4aad9c8d783db7f856e0f31
japius <pmdu93@live.fr>
2017-04-30 15:33:21 +0200

    Fonctionne avec la base de données

--- Login.php
+++ Login.php
@@ -1,9 +1,21 @@
 <?php
 	session_start();
 
-	function findUser(){}
+	function findUser(){
+		$connexion = mysqli_connect ( "pams.script.univ-paris-diderot.fr", "pmejan65", 'd0M%bw5Z' ) ;	
+		MYSQLI_SELECT_DB($connexion, 'pmejan65');
+		if (!$connexion) {
+			echo "Pas de connexion au serveur " ; exit ;	
+		}
+		$req = "SELECT * FROM users WHERE pseudo='".$_POST['pseudo']."';";
+		$reponse = mysqli_query($connexion, $req) or die(mysqli_error($connexion));
+		$tab = mysqli_fetch_assoc($reponse1);
+		return $tab; 
+	}
+
 	$user = findUser();
 	//doit renvoyer false si l'utilisateur n'existe pas et un tableau des données de l'utilisateur si il existe
+	$user = array("id"=>"1", "firstname"=>"askia", "lastname"=>"hamani", "mail"=>"aaa@gmail.com","pseudo"=>"asas","password"=>"azerty");
 	
 	if(!user){header("Location: Connexion.php?error=1");exit;}
 	// Si l'utilisateur n'existe pas renvoie un erreur
