540a30fedd8b7a76d29297d043d13771c9dc48da
firgaty <felix.desmaretz@protonmail.com>
2017-05-05 19:47:55 +0200

    lib_sql_insert_from_post() fait maintenant la distinction en chaines de caractères et variables numériques

--- lib/lib_sql_func.php
+++ lib/lib_sql_func.php
@@ -5,6 +5,8 @@
 */
 
 /**
+* Insere les STRING dans la table BD donnée à partir du $_POST.
+*
 * @param $tab_post : tableau de toutes les champs que l'on veut inserer.
 * @param $link : ce qu'a retourné le mysqli_connect();
 * @param $table : STRING la table SQL dans laquelle on insere les valeurs.
@@ -16,9 +18,14 @@ function lib_sql_insert_from_post($tab_post, $link, $table) {
   foreach($tab_post as $i => $value){
     if(isset($_POST[$value])) {
       $champs = $champs.$value.", ";
-      $sql_values = $sql_values."'".$_POST["$value"]."'".", ";
+      if(is_string($_POST[$value])) {
+        $sql_values = $sql_values."'".$_POST["$value"]."'".", ";
+      } elseif (is_numeric($_POST[$value])) {
+        $sql_values = $sql_values.$_POST["$value"].", ";
+      }
     }
   }
+  if(strlen($champs) == 0 || strlen($sql_values) == 0) return;
 
   $champs = "(".substr($champs, 0 , -2).")";
   $sql_values = "(".substr($sql_values, 0, -2).")";
