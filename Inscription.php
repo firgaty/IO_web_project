534c68a63bd614d6aa33b45a455959493a28d988
japius <pmdu93@live.fr>
2017-05-12 17:31:46 +0200

    Champs ne peuvent pas dépasser 50 caracteres

--- Inscription.php
+++ Inscription.php
@@ -28,12 +28,12 @@
 	?>
 	<form id='inscription' action='VerifInscrip.php' method='POST'>
 	<table>
-	<tr> <td> Nom : </td> <td> <input type='text' name='lastname'></td></tr>
-	<tr> <td> Prénom : </td> <td> <input type='text' name='firstname'></td></tr>
-	<tr> <td> Pseudo : </td> <td> <input type='text' name='pseudo'></td></tr>
-	<tr> <td> Mail : </td> <td> <input type='email' name='mail'></td></tr>
-	<tr> <td> Mot de passe : </td> <td> <input type='password' name='password'></td></tr>
-	<tr> <td> Vérification du mot de passe : </td> <td> <input type='password' name='password2'></td></tr>
+	<tr> <td> Nom : </td> <td> <input type='text' name='lastname' maxlength="50"></td></tr>
+	<tr> <td> Prénom : </td> <td> <input type='text' name='firstname' maxlength="50"></td></tr>
+	<tr> <td> Pseudo : </td> <td> <input type='text' name='pseudo' maxlength="50"></td></tr>
+	<tr> <td> Mail : </td> <td> <input type='email' name='mail' maxlength="50"></td></tr>
+	<tr> <td> Mot de passe : </td> <td> <input type='password' name='password' maxlength="50"></td></tr>
+	<tr> <td> Vérification du mot de passe : </td> <td> <input type='password' name='password2' maxlength="50"></td></tr>
 	</table>
 	<input type='submit' value='Valider mon insciption'>
 	</form>
